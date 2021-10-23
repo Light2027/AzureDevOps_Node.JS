25c78d8400ddd03113a1984cf4691c884a0d694e
Galfi Istvan Balazs <112966@fhwn.ac.at>
2021-10-23 12:43:44 +0200

    Simple app is done; Simple app unit test added;

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
  
 app.get('/', function (_, res) {
-  res.send('Hello World')
+  res.send('Hello World!')
 })
  
 const port = 8080;
 app.listen(port, "", () => console.log(`Listening on ${port}`))
+
+// This should not be a thing...
+// The program itself should not have to be exported for testing...
+module.exports = app;
